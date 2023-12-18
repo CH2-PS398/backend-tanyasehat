62483a4709ba6107fffa7a049b34a95d5afbc2cc
astrocoding <reepalstudio01@gmail.com>
2023-12-18 11:34:01 +0700

    Modified user endpoints

--- routes/auth.js
+++ routes/auth.js
@@ -215,7 +215,7 @@ router.put('/change-password/:id', function (req, res) {
  * GET USER
  */
 
-router.get('/:id', function (req, res) {
+router.get('/users/:id', function (req, res) {
   let id = req.params.id;
 
   connection.query(`SELECT name, email, level, total_pts, exp, created_at FROM users WHERE userId = ${id}`, function (err, rows) {
